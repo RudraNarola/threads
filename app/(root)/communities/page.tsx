2273fce5af93eb63728724d2b6cd667fbe2b34fa
Rudra Narola <114010315+RudraNarola@users.noreply.github.com>
2024-01-24 12:50:43 +0530

    done

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,9 +1,49 @@
-const Page = () => {
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import UserCard from "@/components/cards/UserCard";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+import CommunityCard from "@/components/cards/CommunityCard";
+
+export default async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) {
+    redirect("/onboarding");
+  }
+
+  // fetch all  the community
+  const result = await fetchCommunities({
+    searchString: "",
+  });
+
   return (
     <section>
-      <h1 className="head-text mb-10"> Communities</h1>
+      <h1 className="head-text mb-10"> Search</h1>
+      {/* serach bar */}
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.communities.length === 0 ? (
+          <p className="no-result"> No Community Found</p>
+        ) : (
+          <>
+            {result.communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
-};
-
-export default Page;
+}
